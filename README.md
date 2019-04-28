f69b02df7e71772ee9e0967405306be332ebd2c3
迟海 <chuck@chihai.me>
2019-04-28 18:32:59 +0800

    module changed to class

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-simple-sku
+# simple-sku
+
+Simple sku module for mall items. [Demo](https://chihai-chuck.github.io/simple-sku/demo/index.html)
+
+> 用于线上商城商品的简易sku模块。
+
+**Will be released to npm after the code is perfected.**
--- src/main.js
+++ src/main.js
@@ -1,57 +1,62 @@
-/* eslint-disable */
+class Sku {
+    get [Symbol.toStringTag]() {
+        return "Sku";
+    }
 
-const Sku = function Sku(number) {
-    this.num = parseInt(number); //套装几层结构
-    this.suitRuleInfo = {}; //套装信息 如 {"6692398":"0_26_0_2","6692393":"0_24_0_2"} //{套装ID:第一层_第二层_第三层_第四层}
-    this.paramsSort = []; //已经设置的层级ID及值[0:21,1:0,2:0,3:5]
-    this.paramsArr = []; //每层可选范围[0:[21,11],1:[2]],
-    this.canNotArr = []; //不能选数组，返回给回调函数使用
-    this.tempCan = []; //临时可选
-};
+    constructor(number) {
+        this.num = parseInt(number); // 套装几层结构
+        this.suitRuleInfo = {}; // 套装信息 如 {"6692398":"0_26_0_2","6692393":"0_24_0_2"} //{套装ID:第一层_第二层_第三层_第四层}
+        this.paramsSort = []; // 已经设置的层级ID及值[0:21,1:0,2:0,3:5]
+        this.paramsArr = []; // 每层可选范围[0:[21,11],1:[2]],
+        this.canNotArr = []; // 不能选数组，返回给回调函数使用
+        this.tempCan = []; // 临时可选
+    }
 
-Sku.prototype = {
-    //配置
+    // 配置
     config(obj) {
         this.suitRuleInfo = obj;
         this.init();
-    },
-    //设置信息
+    }
+
+    // 设置信息
     set(position, val) {
         this.paramsSort[position] = val;
         this.singleInit();
         this.out();
-    },
-    //删除信息
+    }
+
+    // 删除信息
     unset(position) {
         this.paramsSort[position] = 0;
         this.singleInit();
         this.out();
-    },
-    //全局初始化
+    }
+
+    // 全局初始化
     init() {
-        //判断套装使用的几层模型
+        // 判断套装使用的几层模型
         if(!this.num) {
             const suitRuleInfo = this.suitRuleInfo;
             for(let i in suitRuleInfo) {
-                if(suitRuleInfo.hasOwnProperty(i)) {
+                if(Object.prototype.hasOwnProperty.call(suitRuleInfo, i)) {
                     if(this.num) break; //只执行一次
                     this.num = suitRuleInfo[i].split("_").length;
                 }
             }
         }
 
-        //根据结构类型创建参数对象
-        for(let i = 0; i < this.num; i++) {
+        // 根据结构类型创建参数对象
+        for(let i=0; i<this.num; i++) {
             this.paramsSort[i] = 0;
             this.paramsArr[i] = [];
         }
 
-        //每层可选范围
+        // 每层可选范围
         for(let i in this.suitRuleInfo) {
-            if(this.suitRuleInfo.hasOwnProperty(i)) {
+            if(Object.prototype.hasOwnProperty.call(this.suitRuleInfo, i)) {
                 const curSuitRuleInfo = this.suitRuleInfo[i].split("_");
                 for(let j in curSuitRuleInfo) {
-                    if(curSuitRuleInfo.hasOwnProperty(j) && !this.paramsArr[j].includes(curSuitRuleInfo[j]) && curSuitRuleInfo[j] > 0) {
+                    if(Object.prototype.hasOwnProperty.call(curSuitRuleInfo, j) && !this.paramsArr[j].includes(curSuitRuleInfo[j]) && curSuitRuleInfo[j] > 0) {
                         this.paramsArr[j].push(curSuitRuleInfo[j]);
                     }
                 }
@@ -59,22 +64,25 @@ Sku.prototype = {
         }
 
         if(typeof this.callBack === "function") this.callBack(Array(this.num).fill([]), void 0);
-    },
-    //临时变量初始化，循环中使用的变量［将每层可选的项置为空］
+    }
+
+    // 临时变量初始化，循环中使用的变量［将每层可选的项置为空］
     tempInit() {
-        for(let i = 0; i < this.num; i++) {
+        for(let i=0; i<this.num; i++) {
             this.tempCan[i] = [];
         }
-    },
-    //单次初始化变量，每次选择变更时要调整的量［将每层不可选的项置为空］
+    }
+
+    // 单次初始化变量，每次选择变更时要调整的量［将每层不可选的项置为空］
     singleInit() {
-        for(let i = 0; i < this.num; i++) {
+        for(let i=0; i<this.num; i++) {
             this.canNotArr[i] = [];
         }
-    },
-    //外部输出
+    }
+
+    // 外部输出
     out() {
-        //判断当前设置了几个层级
+        // 判断当前设置了几个层级
         let number = 0;
         this.paramsSort.forEach((item, index) => {
             if(item || !this.paramsArr[index].length) {
@@ -82,43 +90,41 @@ Sku.prototype = {
             }
         });
 
-        //筛选
+        // 筛选
         this.loopJudge(number);
 
-        //如果所有层级选择完整，且有符合条件的数据
+        // 如果所有层级选择完整，且有符合条件的数据
         let suitId = void 0;
 
         if(number === this.num) {
             const curPattern = this.createPattern(this.paramsSort);
             for(let curSuitId in this.suitRuleInfo) {
-                if(this.suitRuleInfo.hasOwnProperty(curSuitId) && new RegExp(curPattern).test(this.suitRuleInfo[curSuitId])) {
+                if(Object.prototype.hasOwnProperty.call(this.suitRuleInfo, curSuitId) && new RegExp(curPattern).test(this.suitRuleInfo[curSuitId])) {
                     suitId = curSuitId;
                 }
             }
         }
 
-        //返回数据
+        // 返回数据
         this.callBack(this.canNotArr, suitId);
-    },
-    //按需创建正则
+    }
+
+    // 按需创建正则
     createPattern(positionObj) {
         const patternArr = [];
-        for(let i = 0; i < this.num; i++) {
+        for(let i=0; i<this.num; i++) {
             let curVal = "[0-9]+";
             if(i in positionObj) curVal = positionObj[i];
             patternArr.push(curVal);
         }
         return patternArr.join("_");
-    },
-    //递归筛选每个层级不可选 positionArr 位置数组  k当前联合值的个数  number最大联合值的个数
-    loopJudge(number, positionArr, k) {
-        if(!number) return false;
-        if(!positionArr) positionArr = [];
-        if(!k) k = 0;
+    }
 
-        //递归处理数据
-        k = parseInt(k);
-        for(let i = k; i < this.num; i++) {
+    // 递归筛选每个层级不可选 positionArr 位置数组  k当前联合值的个数  number最大联合值的个数
+    loopJudge(number, positionArr = [], index = 0) {
+        if(!number) return false;
+        // 递归处理数据
+        for(let i=index; i<this.num; i++) {
             const curPositionArr = JSON.parse(JSON.stringify(positionArr));
             if(this.paramsSort[i]) {
                 curPositionArr[i] = this.paramsSort[i];
@@ -132,42 +138,43 @@ Sku.prototype = {
         }
 
         return false;
-    },
-    //获取一定不可选的套装数据
+    }
+
+    // 获取一定不可选的套装数据
     judgeSuit(curPattern, positionArr) {
-        //重置临时变量
+        // 重置临时变量
         this.tempInit();
-        //判断正则
+        // 判断正则
         const pattern = new RegExp(curPattern);
         const suitRuleInfo = this.suitRuleInfo;
-        //匹配数据并解析［每个层级可选的项］
+        // 匹配数据并解析［每个层级可选的项］
         for(let i in suitRuleInfo) {
-            if(suitRuleInfo.hasOwnProperty(i)) {
+            if(Object.prototype.hasOwnProperty.call(suitRuleInfo, i)) {
                 let curVal = suitRuleInfo[i];
                 if(pattern.test(curVal)) {
                     const curArr = curVal.split("_");
                     for(let j in curArr) {
-                        if(curArr.hasOwnProperty(j) && !this.tempCan[j].includes(curArr[j])) {
+                        if(Object.prototype.hasOwnProperty.call(curArr, j) && !this.tempCan[j].includes(curArr[j])) {
                             this.tempCan[j].push(curArr[j]);
                         }
                     }
                 }
             }
         }
-        //通过可选推导出各层级一定不能选的ID
+        // 通过可选推导出各层级一定不能选的ID
         for(let i in this.tempCan) {
-            if(this.tempCan.hasOwnProperty(i)) {
+            if(Object.prototype.hasOwnProperty.call(this.tempCan, i)) {
                 if(i in positionArr) continue;
 
                 const canNotArr = this.paramsArr[i].filter(key => !this.tempCan[i].includes(key));
                 for(let j in canNotArr) {
-                    if(canNotArr.hasOwnProperty(j) && !this.canNotArr[i].includes(canNotArr[j])) { //本次正则选择的层级不在范围里
+                    if(Object.prototype.hasOwnProperty.call(canNotArr, j) && !this.canNotArr[i].includes(canNotArr[j])) { //本次正则选择的层级不在范围里
                         this.canNotArr[i].push(canNotArr[j]);
                     }
                 }
             }
         }
     }
-};
+}
 
 export default Sku;
